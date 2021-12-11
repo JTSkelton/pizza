fb41bfb39b8cbabaa9a908e926b74e1ab455bd9c
Jack <skelton.jt9@gmail.com>
2021-12-11 14:48:02 -0500

    Added initial html form

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,38 @@ this.size = size
 this.toppings = toppings;
 }
 
-let pizza1 = new Pizza("Medium",["onions", "mushrooms"])
-console.log(pizza1);
+Pizza.prototype.cost = function(pizzaSize, pizzaToppings){
+let toppingsSum = 0;
+const size = new Map ([
+["Small", 10.00],
+["Medium", 15.00],
+["Large", 20.00]
+]);
+const toppings = new Map ([
+["Pepperoni", 2.00],
+["Sausage", 2.00],
+["Ham", 2.00],
+["Onions", 1.00],
+["Peppers", 1.00],
+["Olives", 1.00],
+["Mushrooms", 2.00],
+["Pineapple", 2.00],
+["Extra Cheese", 2.00]
+]);
+pizzaToppings.forEach(function(element) {
+  toppingsSum += toppings.get(element)
+});
+
+price = size.get(pizzaSize) + toppingsSum;
+return price;
+}
 
 
 //User Interface
+
+$(document).ready(function() {
+  $("#startGame").submit(function(event){
+  event.preventDefault();
+
+  });
+});
